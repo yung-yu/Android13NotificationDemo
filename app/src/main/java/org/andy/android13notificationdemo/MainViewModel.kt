e3ad5a144eb6621aba4d627c601e6fe337e6ee25
andy <yungyu405728@gmail.com>
2022-07-29 11:24:03 +0800

    update all

--- app/src/main/java/org/andy/android13notificationdemo/MainViewModel.kt
+++ app/src/main/java/org/andy/android13notificationdemo/MainViewModel.kt
@@ -11,16 +11,9 @@ class MainViewModel:ViewModel() {
 	private var _permission = MutableStateFlow<Boolean>(false)
 	val permission =_permission.asStateFlow()
 
-	private var _tipPermission = MutableStateFlow<Boolean>(false)
-	val tipPermission =_tipPermission.asStateFlow()
+
 
 	fun checkPermission(activity: MainActivity){
-		val isTip = if (Build.VERSION.SDK_INT >= 33) {
-			activity.shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)
-		} else {
-			false
-		}
-		_tipPermission.value = isTip
 		val isGranted = if (Build.VERSION.SDK_INT >= 33) {
 			ContextCompat.checkSelfPermission(activity, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
 		} else {
